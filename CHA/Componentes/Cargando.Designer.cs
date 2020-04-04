309ab3b7ecd6664877c217e708f41ba04beecb2b
cristian2005 <36666070+cristian2005@users.noreply.github.com>
2020-04-04 12:09:50 -0400

    Agregando El componente cargando...

---
+++ CHA/Componentes/Cargando.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CHA.Componentes
+{
+    partial class Cargando
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cargando));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.color = new System.Windows.Forms.Timer(this.components);
+            this.bunifuColorTransition1 = new Bunifu.Framework.UI.BunifuColorTransition(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bunifuCircleProgressbar1 = new Bunifu.Framework.UI.BunifuCircleProgressbar();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // color
+            // 
+            this.color.Enabled = true;
+            this.color.Interval = 10;
+            this.color.Tick += new System.EventHandler(this.Color_Tick);
+            // 
+            // bunifuColorTransition1
+            // 
+            this.bunifuColorTransition1.Color1 = System.Drawing.Color.White;
+            this.bunifuColorTransition1.Color2 = System.Drawing.Color.White;
+            this.bunifuColorTransition1.ProgessValue = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.bunifuCircleProgressbar1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(201, 236);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(0, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 64);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bunifuCircleProgressbar1
+            // 
+            this.bunifuCircleProgressbar1.animated = true;
+            this.bunifuCircleProgressbar1.animationIterval = 2;
+            this.bunifuCircleProgressbar1.animationSpeed = 1;
+            this.bunifuCircleProgressbar1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuCircleProgressbar1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar1.BackgroundImage")));
+            this.bunifuCircleProgressbar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar1.LabelVisible = false;
+            this.bunifuCircleProgressbar1.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar1.LineThickness = 5;
+            this.bunifuCircleProgressbar1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuCircleProgressbar1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar1.MaxValue = 100;
+            this.bunifuCircleProgressbar1.Name = "bunifuCircleProgressbar1";
+            this.bunifuCircleProgressbar1.ProgressBackColor = System.Drawing.Color.Transparent;
+            this.bunifuCircleProgressbar1.ProgressColor = System.Drawing.Color.SeaGreen;
+            this.bunifuCircleProgressbar1.Size = new System.Drawing.Size(149, 149);
+            this.bunifuCircleProgressbar1.TabIndex = 2;
+            this.bunifuCircleProgressbar1.Value = 30;
+            // 
+            // Cargando
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.panel1);
+            this.Enabled = false;
+            this.Name = "Cargando";
+            this.Size = new System.Drawing.Size(201, 236);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer color;
+        private Bunifu.Framework.UI.BunifuColorTransition bunifuColorTransition1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuCircleProgressbar bunifuCircleProgressbar1;
+    }
+}
---
+++ CHA/Componentes/Cargando.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CHA.Componentes
+{
+    /// <summary>
+    /// Representa un circulo de colores que lo puede personalizar a tu gusto.
+    /// </summary>
+    public partial class Cargando : UserControl
+    {
+        private List<string> texto = new List<string>();
+        private Timer textotime = new Timer();
+        private int ocultartime = 0;
+        /// <summary>
+        /// Lista de colores que tendra el cargando(por defecto hay 3 colores y es obligatorio tener uno).
+        /// </summary>
+        public List<Color> ColoresCargando { get; set; }
+        /// <summary>
+        /// Agrega un texto al cargando, si no quieres texto dejalo vacio.
+        /// </summary>
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+
+        [Editor("System.Windows.Forms.Design.StringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", typeof(System.Drawing.Design.UITypeEditor))]
+        public List<string> Texto
+        {
+            get
+            {
+                return texto;
+            }
+            set { texto = value; }
+        }
+        /// <summary>
+        /// Representa el color del texto.
+        /// </summary>
+        public Color ColorTexto { get { return label1.ForeColor; } set { this.label1.ForeColor = value; } }
+        /// <summary>
+        /// Representa el Intervalo que durará cada listado del Texto.
+        /// </summary>
+        public int IntervaloTexto { get { return this.textotime.Interval; } set { this.textotime.Interval = value; } }
+        /// <summary>
+        /// Representa el tiempo que durara el cargando.
+        /// </summary>
+        public int IntervaloOcultarCargando { get { return ocultartime; } set { ocultartime = value; } }
+        int color_ = 0;
+        int text_ = 0;
+        public Cargando()
+        {
+            this.textotime.Enabled = true;
+            this.textotime.Tick += new System.EventHandler(this.Textotime_Tick);
+            InitializeComponent();
+            ColoresCargando = new List<Color>();
+            ColorTexto = new Color();
+
+
+            label1.ForeColor = ColorTexto;
+            ColoresCargando.Add(Color.FromArgb(0, 150, 136));
+            ColoresCargando.Add(Color.FromArgb(0, 188, 212));
+            ColoresCargando.Add(Color.FromArgb(63, 81, 181));
+            ColoresCargando.Add(Color.FromArgb(156, 39, 176));
+            bunifuCircleProgressbar1.ProgressColor = ColoresCargando[0];
+            color.Start();
+        }
+        int dir = 1;
+        int i = 0;
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (IntervaloOcultarCargando != 0 && i == IntervaloOcultarCargando - 1000)
+            {
+                this.Dispose();
+            }
+            i += 10;
+            if (bunifuCircleProgressbar1.Value == 100)
+            {
+                dir = -1;
+                bunifuCircleProgressbar1.animationIterval = 5;
+                SwictColor();
+            }
+            else if (bunifuCircleProgressbar1.Value == 10)
+            {
+                dir = +1;
+                bunifuCircleProgressbar1.animationIterval = 2;
+                SwictColor();
+            }
+            bunifuCircleProgressbar1.Value += dir;
+
+        }
+        void SwictColor()
+        {
+            bunifuColorTransition1.Color1 = ColoresCargando[color_];
+            if (color_ < ColoresCargando.Count - 1)
+                color_++;
+            else
+                color_ = 0;
+            bunifuColorTransition1.Color1 = ColoresCargando[color_];
+            color.Start();
+        }
+
+        private void Color_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ColoresCargando.Count > 0)
+                {
+                    bunifuColorTransition1.Color1 = ColoresCargando[color_];
+                    if (bunifuColorTransition1.ProgessValue < 80)
+                    {
+                        bunifuColorTransition1.ProgessValue++;
+                        bunifuCircleProgressbar1.ProgressColor = bunifuColorTransition1.Value;
+                    }
+                    else
+                    {
+                        color.Stop();
+                        bunifuColorTransition1.Color1 = bunifuColorTransition1.Color2;
+                        bunifuColorTransition1.ProgessValue = 0;
+                    }
+                }
+                else
+                {
+                    ColoresCargando.Add(Color.FromArgb(0, 150, 136));
+                    MessageBox.Show("Debes de asignar al menos un color", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception error)
+            {
+                ColoresCargando.Add(Color.FromArgb(0, 150, 136));
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Textotime_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = (Texto.Count > 0) ? Texto[text_] : "";
+                if (Texto.Count - 1 > text_)
+                {
+                    text_++;
+                }
+                else if (Texto.Count == text_)
+                    textotime.Stop();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
